448108a6b40aaa196255768788a26734e7dfb101
Volodymyr Domikan <v.domikan@bbq.agency>
2021-11-28 23:32:04 +0200

    changes

--- src/test/java/io/ctdev/web/tests/VerifyUserCanAddProductToBasketTest.java
+++ src/test/java/io/ctdev/web/tests/VerifyUserCanAddProductToBasketTest.java
@@ -10,7 +10,6 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import java.lang.*;
 
 import static io.ctdev.framework.driver.WebDriverSingleton.getDriver;
 import static java.lang.String.format;
@@ -34,7 +33,6 @@ public class VerifyUserCanAddProductToBasketTest {
     @Description("Verify user can login")
     public void verifyLogIn() {
         loginPage.logInAsCustomer(customer);
-        Thread.sleep(60000);
 
         Assert.assertTrue(loginPage.isAccountIconPresent(),
                 "Customer Icon should be present");
