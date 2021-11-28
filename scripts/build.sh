69de4524bcd3335f17f9ddf024118cd66e8694f7
Volodymyr Domikan <v.domikan@bbq.agency>
2021-11-28 20:26:41 +0200

    changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,6 @@
 
 docker network create selenoid --subnet 172.37.40.0/24
 docker pull selenoid/vnc:chrome_91.0
-docker build -t maven . -f image/maven-ci/Dockerfile
+
+#docker build -t maven . -f image/maven-ci/Dockerfile
+#docker build -t maven-tests . -f image/maven-ci/Dockerfile
