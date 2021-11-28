97b986ee0b7d098402fc8396baeb3598b89de846
Volodymyr Domikan <v.domikan@bbq.agency>
2021-11-29 01:04:33 +0200

    changes

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,6 +3,6 @@
 set -e
 EXIT_CODE=0
 
-docker run --rm --net selenoid --name maven -v $PWD/target:/app/target/ dva1986/maven-tests clean test -e -X || EXIT_CODE=$?
+docker run --rm --net selenoid --name maven -v $PWD/target:/app/target/ dva1986/maven-tests test -e -X || EXIT_CODE=$?
 
 echo $EXIT_CODE
