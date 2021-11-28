1d10f8df0d59a57aa2c6a99bbeecfefc53a9a83f
Volodymyr Domikan <v.domikan@bbq.agency>
2021-11-28 21:45:09 +0200

    changes

--- scripts/run-selenoid-ui.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d --net selenoid --name selenoid-ui -p 8081:8080 aerokube/selenoid-ui:latest-release --selenoid-uri http://selenoid:4444
--- scripts/run-selenoid.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d --net selenoid --name selenoid -p 4445:4444 -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/:/etc/selenoid/:ro aerokube/selenoid:latest-release -conf /etc/selenoid/browsers.json -video-output-dir /opt/selenoid/video/ -timeout 3m0s -container-network selenoid
