ffa5894bb14a52f61c59e000c9f55f548020b5d4
Volodymyr Domikan <v.domikan@bbq.agency>
2021-11-29 22:34:35 +0200

    changes

--- src/test/java/io/ctdev/framework/pages/login/LoginPage.java
+++ src/test/java/io/ctdev/framework/pages/login/LoginPage.java
@@ -162,7 +162,7 @@ public class LoginPage extends AbstractPage {
     }
 
     public void waitForSomething(long timeInSeconds) {
-        System.out.println("WAIT (REBUILD IMAGE) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("WAIT (REBUILD IMAGE #1) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         wait(basketProductName, timeInSeconds);
     }
 }
